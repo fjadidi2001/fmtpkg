cf93c037fb917b0452671a6c852436b2579c1b78
fatmeme jadidi <fjadidi2001@gmail.com>
2022-07-17 10:03:53 -0400

    second edit

--- 6fmt.go
+++ 6fmt.go
@@ -8,15 +8,17 @@ import (
 func main() {
 	var num1 = 23
 	const num2 = 10
-	var myString = "Hello means salam"
+	var numberi = "Hello means salam"
 	var work = " Nice job"
-	fmt.Scanln(&myString) //be sorat pointer ersal mikonim
-	num3, err := strconv.Atoi(myString)
+	var sima = "raftam"
+	fmt.Scanln(&numberi) //pointer format
+	num3, err := strconv.Atoi(numberi)
 	if err != nil {
 		fmt.Println("ye number vared kon")
 		return
 	}
 	num4 := -4 + 3i
+	num5 := 25.36
 	if num1 < num2 {
 		fmt.Printf("this is %c", num2)
 	} else {
@@ -25,11 +27,11 @@ func main() {
 		fmt.Printf("base 16 num 1 is %x\n", num1)
 		fmt.Printf("base 16 num2 is %X\n\n", num2)
 		fmt.Printf("base 16 num3 is %X\n", num3)
-		fmt.Printf("string is %s\n", myString)
+		fmt.Printf("string is %s\n", work)
 		fmt.Printf("try to use %q\n", work)
-		fmt.Printf("format for Character %c\n", work)
+		fmt.Printf("format for Character %c\n", sima)
 		fmt.Printf("format for unicode: %U\n", num2)
 		fmt.Printf("format for scientific notation%E\n ", num4)
-		fmt.Printf("float")
+		fmt.Printf("float format %.2f", num5)
 	}
 }
